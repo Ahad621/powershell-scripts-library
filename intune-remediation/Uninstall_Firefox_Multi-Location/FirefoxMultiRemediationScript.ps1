649d1ad8170e15d30c006911d5f70f4a455bdb7a
Ahad Alam <132224896+Ahad621@users.noreply.github.com>
2025-05-02 05:12:08 +0530

    Added MS Store App Removal to FirefoxMultiRemediationScript.ps1

--- intune-remediation/Uninstall_Firefox_Multi-Location/FirefoxMultiRemediationScript.ps1
+++ intune-remediation/Uninstall_Firefox_Multi-Location/FirefoxMultiRemediationScript.ps1
@@ -4,50 +4,66 @@
 ## Date: 10-April-2025
 
 
-$LocalUsers = (Get-ChildItem -Path "C:\Users").name
+$LocalUsers = (Get-ChildItem -Path "C:\Users" -Directory).Name
 
-# Uninstalling from Program Files
-if (Test-Path "${env:ProgramFiles(x86)}\Mozilla Firefox\uninstall\helper.exe"){
-    Start-Process -FilePath "${env:ProgramFiles(x86)}\Mozilla Firefox\uninstall\helper.exe" -ArgumentList '/S' -Verbose -ErrorAction SilentlyContinue
-}
-
-if (Test-Path "${env:ProgramFiles}\Mozilla Firefox\uninstall\helper.exe"){
-    Start-Process -FilePath "${env:ProgramFiles}\Mozilla Firefox\uninstall\helper.exe" -ArgumentList '/S' -Verbose -ErrorAction SilentlyContinue
-}
-
-# Uninstalling for each user
-ForEach ($LocalUser in $LocalUsers){
-    $Userpath = "C:\Users\" + $LocalUser
-
-    if (Test-Path "$Userpath\AppData\Local\Mozilla Firefox\uninstall\helper.exe"){
-        Start-Process -FilePath "$Userpath\AppData\Local\Mozilla Firefox\uninstall\helper.exe" -ArgumentList '/S' -Verbose -ErrorAction SilentlyContinue
+# Function to uninstall Firefox from a given path
+function Uninstall-Firefox {
+    param ([string]$Path)
+    if (Test-Path "$Path\uninstall\helper.exe") {
+        Write-Output "Attempting to uninstall Firefox from: $Path"
+        Start-Process -FilePath "$Path\uninstall\helper.exe" -ArgumentList '/S' -Wait -ErrorAction SilentlyContinue
     }
-    Start-Sleep 20
-
-    # Remove shortcuts from appdata
-    Remove-Item -Path "$userpath\AppData\Local\Mozilla" -Force -Recurse -Verbose -ErrorAction SilentlyContinue
-    Remove-Item -Path "$userpath\AppData\LocalLow\Mozilla" -Force -Recurse -Verbose -ErrorAction SilentlyContinue
-    Remove-Item -Path "$userpath\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Firefox.lnk" -Force -Verbose -ErrorAction SilentlyContinue
-    Remove-Item -Path "$userpath\desktop\firefox.lnk" -Force -Verbose -ErrorAction SilentlyContinue
 }
-    # Remove related registry keys
-    $pathToRemove = @(
-    'HKLM:\Software\Mozilla'
-    'HKLM:\SOFTWARE\mozilla.org'
-    'HKLM:\SOFTWARE\MozillaPlugins'
-    'HKLM:\SOFTWARE\WOW6432Node\Mozilla'
-    'HKLM:\SOFTWARE\WOW6432Node\mozilla.org'
-    'HKLM:\SOFTWARE\WOW6432Node\MozillaPlugins'
-    'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Firefox.lnk'
-    )
 
-    foreach($path in $pathToRemove) {
-        if(Test-Path $path) {
-            try {
-                Remove-Item $path -Recurse -Force -Verbose -ErrorAction SilentlyContinue
-            }
-            catch {
-                    Write-Warning $_.Exception.Message
-            }
+# Uninstall from common program locations
+Uninstall-Firefox "${env:ProgramFiles(x86)}\Mozilla Firefox"
+Uninstall-Firefox "${env:ProgramFiles}\Mozilla Firefox"
+
+# Uninstall from each user’s local AppData
+foreach ($LocalUser in $LocalUsers) {
+    $UserPath = "C:\Users\$LocalUser"
+    $LocalInstall = "$UserPath\AppData\Local\Mozilla Firefox"
+    $RoamingInstall = "$UserPath\AppData\Roaming\Mozilla Firefox"
+
+    Uninstall-Firefox $LocalInstall
+    Uninstall-Firefox $RoamingInstall
+
+    Start-Sleep -Seconds 10
+
+    # Remove Mozilla folders
+    Remove-Item "$UserPath\AppData\Local\Mozilla" -Recurse -Force -Verbose -ErrorAction SilentlyContinue
+    Remove-Item "$UserPath\AppData\LocalLow\Mozilla" -Recurse -Force -Verbose -ErrorAction SilentlyContinue
+    Remove-Item "$UserPath\AppData\Roaming\Mozilla Firefox" -Recurse -Force -Verbose -ErrorAction SilentlyContinue
+
+    # Remove Firefox shortcuts
+    Remove-Item "$UserPath\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Firefox.lnk" -Force -Verbose -ErrorAction SilentlyContinue
+    Remove-Item "$UserPath\Desktop\firefox.lnk" -Force -Verbose -ErrorAction SilentlyContinue
+}
+
+# Remove global shortcuts and registry traces
+$pathsToRemove = @(
+'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Firefox.lnk',
+'HKLM:\Software\Mozilla',
+'HKLM:\Software\mozilla.org',
+'HKLM:\Software\MozillaPlugins',
+'HKLM:\Software\WOW6432Node\Mozilla',
+'HKLM:\Software\WOW6432Node\mozilla.org',
+'HKLM:\Software\WOW6432Node\MozillaPlugins'
+)
+
+foreach ($path in $pathsToRemove) {
+    if (Test-Path $path) {
+        try {
+            Remove-Item $path -Recurse -Force -Verbose -ErrorAction SilentlyContinue
+        } catch {
+            Write-Warning $_.Exception.Message
         }
+    }
+}
+
+# Uninstall Firefox if installed from the Microsoft Store
+$StoreApp = Get-AppxPackage -Name "Mozilla.Firefox*"
+if ($StoreApp) {
+    Write-Output "Uninstalling Firefox installed via Microsoft Store"
+    Remove-AppxPackage -Package $StoreApp.PackageFullName
 }
